30f251366ba0778acfd8ab42dceb5b80929dd20f
Fabrizio <fhf.lic17@uea.edu.br>
2020-10-14 14:48:52 -0300

    feat: telas e botões

---
+++ EcoLogic/Assets/Scripts/Storyboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Storyboard : MonoBehaviour
+{
+    
+    private int count;
+    public Image actual;
+    public List<Sprite> images;
+
+    void Start(){
+        count = 0;
+    }
+
+    public void switch_image(){
+        if(count < 3) actual.sprite = images[count];
+        count++;
+        if(count == 4) SceneManager.LoadScene("Fases");
+    }
+    
+}
