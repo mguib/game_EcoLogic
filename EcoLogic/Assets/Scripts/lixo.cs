b201c5eb8415a3009c295c1e219837ac4324d6f0
mguib <43528733+mguib@users.noreply.github.com>
2020-10-16 04:05:46 -0400

    feat: Organização dos lixos e lixeiras nas fases

---
+++ EcoLogic/Assets/Scripts/lixo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lixo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Lata")
+        {
+            //SceneManager.LoadScene(levelName);
+            Destroy(gameObject);
+        }
+    }
+}
