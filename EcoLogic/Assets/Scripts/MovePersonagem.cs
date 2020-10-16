cbc5f2537eb92ce8225f391398181d70fbc41f65
Fabrizio <fhf.lic17@uea.edu.br>
2020-10-16 17:06:17 -0300

    feat: animações Sofia

--- EcoLogic/Assets/Scripts/MovePersonagem.cs
+++ EcoLogic/Assets/Scripts/MovePersonagem.cs
@@ -9,6 +9,8 @@ public class MovePersonagem : MonoBehaviour
     //public Animator anim;
     public string dir;
 
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,26 +38,34 @@ public class MovePersonagem : MonoBehaviour
         {
             direcao += Vector2.up;
             transform.Translate(new Vector2(0,vel * Time.deltaTime));
+            animator.SetFloat("Idle",1f);
             
         }
         if (Baixo.pressionando)
         {
             direcao += Vector2.down;
             transform.Translate(new Vector2(0, -1 * vel * Time.deltaTime));
+            animator.SetFloat("Idle",2f);
             
         }
         if (Direita.pressionando)
         {
             direcao += Vector2.right;
             transform.Translate(new Vector2(vel * Time.deltaTime, 0));
+            animator.SetFloat("Idle",3f);
             
         }
         if (Esquerda.pressionando)
         {
             direcao += Vector2.left;
             transform.Translate(new Vector2( -1* vel * Time.deltaTime, 0));
+            animator.SetFloat("Idle",4f);
             
         }
+
+        animator.SetFloat("Horizontal",direcao.x);
+        animator.SetFloat("Vertical",direcao.y);
+        animator.SetFloat("Speed",direcao.sqrMagnitude);
     }
     
 
